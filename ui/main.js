466a8736960c971ffd095689ff326191038f2de7
Aswin G <aswingauthor@gmail.com>
2017-08-06 00:32:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,12 @@
-var request=new XMLHttpRequest();
-request.onreadystatechange=function(){
-    if(request.readystate==XMLHttpRequest.DONE && request.status==200){
-        counter=request.responseText();
-        document.getElementById('count').innerHTML=counter.toString();
+function requester(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate==XMLHttpRequest.DONE && request.status==200){
+            counter=request.responseText();
+            document.getElementById('count').innerHTML=counter.toString();
+        }
     }
+    
+    request.open('GET','http://aswinganesh666.imad.hasura-app.io/count',false);
+    request.send();
 }
-
-request.open('GET','http://aswinganesh666.imad.hasura-app.io/count',false);
-request.send();
