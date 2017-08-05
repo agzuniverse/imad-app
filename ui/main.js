35103a5f53c49d2d066826720dc196ae2e3ae4f3
Aswin G <aswingauthor@gmail.com>
2017-08-06 00:37:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ function requester(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         console.log("state changed!")
-        if(request.readystate==XMLHttpRequest.DONE && request.status==200){
+        if(request.readyState===XMLHttpRequest.DONE && request.status===200){
             console.log("request done succesfully");
             counter=request.responseText();
             document.getElementById('count').innerHTML=counter.toString();
