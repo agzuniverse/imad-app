0e07fef4bbc50cbd6b29c14deefc58e6e44ae63c
Aswin G <aswingauthor@gmail.com>
2017-08-06 00:30:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
-console.log('Loaded!');
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+    if(request.readystate==XMLHttpRequest.DONE && request.status==200){
+        counter=request.responseText();
+        document.getElementById('count').innerHTML=counter.toString();
+    }
+}
+
+request.open('GET','http://aswinganesh666.imad.hasura-app.io/count',false);
+request.send();
