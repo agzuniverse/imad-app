2e37f290d144987ee1c5bbaaabe29a208517449f
Aswin G <aswinganesh666@gmail.com>
2017-08-24 23:06:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,14 @@ function requester(){
     request.onreadystatechange=function(){
         console.log("state changed!")
         if(request.readyState==XMLHttpRequest.DONE && request.status==200){
-            console.log("request done succesfully");
-            counter=request.responseText;
-            document.getElementById('count').innerHTML=counter.toString();
+            console.log("USER LOGGED IN");
+            alert("success!");
         }
     }
     
-    request.open('GET','http://aswinganesh666.imad.hasura-app.io/count',true);
-    request.send();
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    request.open('POST','http://aswinganesh666.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
 }
